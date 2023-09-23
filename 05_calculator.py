e7171605dec820329e0512073e9de4c96a8d7871
soumyadeep-pradhan <soumyadeeppradhan15@gmail.com>
2023-09-23 23:18:20 +0530

    modification

--- 05_calculator.py
+++ 05_calculator.py
@@ -16,6 +16,18 @@ def button_click(number):
 def button_clear():
     e.delete(0,END)
 
+def button_add():
+    first_number = e.get()
+    global f_num 
+    f_num = int(first_number)
+    e.delete(0,END)
+    
+def button_equal():
+    second_number = e.get()
+    e.delete(0,END)
+    e.insert(0,f_num+int(second_number))
+    
+
 button0 = Button(root, text="0",padx=40,pady=20, command=lambda: button_click(0))
 button1 = Button(root, text="1",padx=40,pady=20, command=lambda: button_click(1))
 button2 = Button(root, text="2",padx=40,pady=20, command=lambda: button_click(2))
@@ -26,8 +38,8 @@ button6 = Button(root, text="6",padx=40,pady=20, command=lambda: button_click(6)
 button7 = Button(root, text="7",padx=40,pady=20, command=lambda: button_click(7))
 button8 = Button(root, text="8",padx=40,pady=20, command=lambda: button_click(8))
 button9 = Button(root, text="9",padx=40,pady=20, command=lambda: button_click(9))
-buttonSum = Button(root, text="+", padx=39, pady=20,command=lambda: button_click())
-buttonEqual = Button(root, text="=", padx=91, pady=20,command=lambda: button_click())
+buttonSum = Button(root, text="+", padx=39, pady=20,command=button_add)
+buttonEqual = Button(root, text="=", padx=91, pady=20,command=button_equal)
 buttonClear = Button(root, text="clear", padx=79, pady=20,command=button_clear)
 #put buttons on screen
 
@@ -39,9 +51,9 @@ button4.grid(row=2 , column=0)
 button5.grid(row=2 , column=1)
 button6.grid(row=2 , column=2)
 
-button7.grid(row=3, column=0)
-button8.grid(row=3, column=1)
-button9.grid(row=3, column=2)
+button7.grid(row=1, column=0)
+button8.grid(row=1, column=1)
+button9.grid(row=1, column=2)
 
 button0.grid(row=4, column=0)
 buttonClear.grid(row=4, column=1, columnspan=2)
