6f2ac57b304aba15742927001f5f208571cdb93a
soumyadeep-pradhan <soumyadeeppradhan15@gmail.com>
2023-09-29 21:59:34 +0530

    new

--- 05_calculator.py
+++ 05_calculator.py
@@ -16,6 +16,56 @@ def button_click(number):
 def button_clear():
     e.delete(0,END)
 
+def button_sum():
+	first_number = e.get()
+	global f_num
+	global math
+	math = "addition"
+	f_num = int(first_number)
+	e.delete(0, END)
+
+def button_equal():
+	second_number = e.get()
+	e.delete(0, END)
+	
+	if math == "addition":
+		e.insert(0, f_num + int(second_number))
+
+	if math == "subtraction":
+		e.insert(0, f_num - int(second_number))
+
+	if math == "multiplication":
+		e.insert(0, f_num * int(second_number))
+
+	if math == "division":
+		e.insert(0, f_num / int(second_number))
+  
+    
+def button_subtract():
+	first_number = e.get()
+	global f_num
+	global math
+	math = "subtraction"
+	f_num = int(first_number)
+	e.delete(0, END)
+ 
+def button_multiply():
+	first_number = e.get()
+	global f_num
+	global math
+	math = "multiplication"
+	f_num = int(first_number)
+	e.delete(0, END)
+ 
+def button_divide():
+	first_number = e.get()
+	global f_num
+	global math
+	math = "division"
+	f_num = int(first_number)
+	e.delete(0, END)
+
+
 button0 = Button(root, text="0",padx=40,pady=20, command=lambda: button_click(0))
 button1 = Button(root, text="1",padx=40,pady=20, command=lambda: button_click(1))
 button2 = Button(root, text="2",padx=40,pady=20, command=lambda: button_click(2))
@@ -26,10 +76,17 @@ button6 = Button(root, text="6",padx=40,pady=20, command=lambda: button_click(6)
 button7 = Button(root, text="7",padx=40,pady=20, command=lambda: button_click(7))
 button8 = Button(root, text="8",padx=40,pady=20, command=lambda: button_click(8))
 button9 = Button(root, text="9",padx=40,pady=20, command=lambda: button_click(9))
-buttonSum = Button(root, text="+", padx=39, pady=20,command=lambda: button_click())
-buttonEqual = Button(root, text="=", padx=91, pady=20,command=lambda: button_click())
+buttonSum = Button(root, text="+", padx=39, pady=20,command=button_sum)
+buttonEqual = Button(root, text="=", padx=91, pady=20,command=button_equal)
 buttonClear = Button(root, text="clear", padx=79, pady=20,command=button_clear)
+
+button_subtract = Button(root, text = '-', padx=41,pady=20, command=button_subtract)
+button_multiply = Button(root, text = '*', padx=40,pady=20, command=button_multiply)
+button_divide = Button(root, text = '/', padx=40,pady=20, command=button_divide)
+
 #put buttons on screen
+
+
 
 button1.grid(row= 3, column=0)
 button2.grid(row= 3, column=1)
@@ -47,5 +104,9 @@ button0.grid(row=4, column=0)
 buttonClear.grid(row=4, column=1, columnspan=2)
 buttonSum.grid(row=5 ,column=0)
 buttonEqual.grid(row=5, column=1, columnspan=2)
+
+button_subtract.grid(row=6, column=0)
+button_multiply.grid(row=6, column=1)
+button_divide.grid(row=6, column=2)
 
 root.mainloop()
